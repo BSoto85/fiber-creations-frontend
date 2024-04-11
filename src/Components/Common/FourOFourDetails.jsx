2ef07665cbde45e04e10c1c3bbae46a4efa1722d
brenda-soto <bsoto@pursuit.org>
2024-04-11 16:08:19 -0400

    Started on 404 page.

--- src/Components/Common/FourOFourDetails.jsx
+++ src/Components/Common/FourOFourDetails.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./FourOFourDetails.css";
 
 const FourOFourDetails = () => {
-  return <div>FourOFourDetails</div>;
+  return (
+    <div className="container">
+      <h2>Oops! You used the wrong yarn!</h2>
+      <img src="https://cdn.svgator.com/images/2022/01/cat.png" alt="" />
+      <Link to={"/creations"}>
+        <button>Back</button>
+      </Link>
+    </div>
+  );
 };
 
 export default FourOFourDetails;
